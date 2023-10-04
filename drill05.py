b312284bcefd0da92a731f6d43e05fb4db750442
xorud1210 <rhxo1210@naver.com>
2023-10-04 14:31:22 +0900

    x, y 를 cx, cy로 변수 이름 변경

--- drill05.py
+++ drill05.py
@@ -26,10 +26,10 @@ def handle_events():
 
 
 def reset_world():
-    global running, x, y, frame
+    global running, cx, cy, frame
     global hx, hy
     running = True
-    x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+    cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
 
     # hx, hy = TUK_WIDTH -50, TUK_HEIGHT - 50
@@ -40,7 +40,7 @@ def redner_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(hx, hy)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
     update_canvas()
 
 
